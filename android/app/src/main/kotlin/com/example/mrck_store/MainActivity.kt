d9fd85f3aa54094a44127407674fdd5a69ba1410
MrCK <rashidmrck@gmail.com>
2020-07-29 03:58:43 +0530

    starting

---
+++ android/app/src/main/kotlin/com/example/mrck_store/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mrck_store
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "MrCK Stor",
+    );
+  }
+}
